6dc91fc915b95cf0fbe625f2e98de11eb1731f10
Nisarg <nisargkachhia@gmail.com>
2024-11-05 22:45:22 -0600

    Update Main file

--- dynamicarr.py
+++ dynamicarr.py
@@ -1,70 +1,79 @@
 import ctypes
 
-class DynamicList:
+class DynamicArray:
     def __init__(self):
-        # Start with a small array capacity of 1 and no items
-        self._count = 0
-        self._storage = self._new_array(1)
+        # Initialize with a capacity of 1 and no elements
+        self._size = 0
+        self._capacity = 1
+        self._array = self._create_array(self._capacity)
 
     def __len__(self):
-        # Number of items currently in the list
-        return self._count
+        # Return the number of elements in the array
+        return self._size
 
     def __repr__(self):
-        # Return a readable version of the list contents
-        return f"DynamicList({[self.get_item(i) for i in range(self._count)]})"
+        # Return a readable string representation of the array contents
+        return f"DynamicArray({[self.get_item(i) for i in range(self._size)]})"
 
     def append(self, item):
-        # Add a new item to the list, expanding capacity if needed
-        if self._count == len(self._storage):
-            self._resize(2 * len(self._storage))
-        self._storage[self._count] = item
-        self._count += 1
+        # Add an item to the array, resizing if necessary
+        if self._size == self._capacity:
+            self._resize(2 * self._capacity)
+        self._array[self._size] = item
+        self._size += 1
 
     def get_item(self, index):
-        # Fetch an item at a specific index
-        if not 0 <= index < self._count:
-            raise IndexError("Index is out of range.")
-        return self._storage[index]
+        # Retrieve the item at a given index
+        if not 0 <= index < self._size:
+            raise IndexError("Index out of range.")
+        return self._array[index]
 
     def remove_item(self, item):
-        # Remove the first occurrence of an item from the list
-        for i in range(self._count):
-            if self._storage[i] == item:
-                for j in range(i, self._count - 1):
-                    self._storage[j] = self._storage[j + 1]
-                self._count -= 1
+        # Remove the first occurrence of an item, resizing if necessary
+        for i in range(self._size):
+            if self._array[i] == item:
+                # Shift elements to fill the gap
+                for j in range(i, self._size - 1):
+                    self._array[j] = self._array[j + 1]
+                self._size -= 1
+                # Shrink capacity if necessary
+                if self._size < self._capacity // 4:
+                    self._resize(self._capacity // 2)
                 return
         raise ValueError("Item not found in list.")
 
-    def _new_array(self, capacity):
-        # Create a new array with the specified capacity
+    def _create_array(self, capacity):
+        # Create a new array with the given capacity
         return (ctypes.c_int * capacity)()
 
     def _resize(self, new_capacity):
-        # Resize the internal storage to a new capacity
-        new_storage = self._new_array(new_capacity)
-        for i in range(self._count):
-            new_storage[i] = self._storage[i]
-        self._storage = new_storage
+        # Resize the internal storage to the new capacity
+        if new_capacity < 1:
+            new_capacity = 1
+        new_array = self._create_array(new_capacity)
+        # Copy items to the new array
+        for i in range(self._size):
+            new_array[i] = self._array[i]
+        self._array = new_array
+        self._capacity = new_capacity
 
 # Usage example
 if __name__ == "__main__":
-    my_list = DynamicList()
-    my_list.append(10)
-    my_list.append(20)
-    my_list.append(30)
-    my_list.append(40)
-    my_list.append(50)
+    my_array = DynamicArray()
+    my_array.append(10)
+    my_array.append(20)
+    my_array.append(30)
+    my_array.append(40)
+    my_array.append(50)
 
-    print("List length:", len(my_list))
-    print("Contents of the list:", my_list)
+    print("Array length:", len(my_array))
+    print("Contents of the array:", my_array)
 
-    my_list.append(60)
-    print("List after appending 60:", my_list)
-    print("List length after appending:", len(my_list))
-    print("Item at index 2:", my_list.get_item(2))
+    my_array.append(60)
+    print("Array after appending 60:", my_array)
+    print("Array length after appending:", len(my_array))
+    print("Item at index 2:", my_array.get_item(2))
 
-    my_list.remove_item(10)
-    print("List after removing 10:", my_list)
-    print("List length after removing:", len(my_list))
+    my_array.remove_item(10)
+    print("Array after removing 10:", my_array)
+    print("Array length after removing:", len(my_array))
